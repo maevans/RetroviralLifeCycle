c2453931a52f37aa628acf838be1ab6b4dcbb6e4
Mahalia E <maev8907@colorado.edu>
2020-11-18 15:29:38 -0700

    Add Arrows & Key Instructions

--- VirusMain.cpp
+++ VirusMain.cpp
@@ -17,7 +17,13 @@
 #define WIDTH 600
 #define HEIGHT 600
 // ---
-double ratio = WIDTH/(double)HEIGHT; //Aspect Ratio
+double ratio = WIDTH/(double)HEIGHT; // Aspect Ratio
+double world = 5.0;                  // Dimensions
+double field = 45;                   // Field Of View
+float view_ortho = 1;                // Ortho View
+int modeV = 0;                       // Mode of View
+double angle = 0;
+double elev = 0; 
 // ---
 
 /*
@@ -59,6 +65,20 @@ double ratio = WIDTH/(double)HEIGHT; //Aspect Ratio
  */
 
 
+//--------ORTHO/PERS--------
+void View() {
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    if (modeV){
+        //printf("Field: %f \n",field);
+       gluPerspective(field, ratio, world/4, 4*world);  //  Perspective - Angle, Aspect Ratio, Min, Max
+    }
+    else
+       glOrtho(-ratio*world*view_ortho, +ratio*world*view_ortho, -ratio*world*view_ortho, +ratio*world*view_ortho, -10, +10);  //  Orthogonal projection
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+}
+
 
 /*
  *   DISPLAY
@@ -71,29 +91,73 @@ void display(){
     
     glLoadIdentity();                                    // Undo Previous Transformation
     
-    glTranslated(0, 0, 0);
+    View();
+        
+        // --- CAMERA ---
+        //--------------------------
+        if (modeV)                                          //  Perspective - Eye View
+         {
+           gluLookAt(0,0,8, 0,0,0, 0,1,0);
+           glTranslated(0, 0, 0);
+           glRotatef(elev, 1, 0, 0);
+           glRotatef(angle, 0, -1, 0);
+         }
+        else                                                //  Orthogonal - World View
+         {
+           glTranslated(1, 0, -4);
+           gluLookAt(0,1,0, 1,0,-4, 0,4,0);
+           glRotatef(elev, 1, 0, 0);
+           glRotatef(angle, 0, 1, 0);
+         }
+    
+    
+    //glTranslated(0, 0, 0);
     
     // Draw Virus
-    glTranslated(0.2, 0.8, 0);
+    glPushMatrix();
+    glTranslated(-2, 5, 0);
     glColor3f(0.2, 0.7, 0.3);       // Green - 0.2/0.7/0.3
-    glutSolidSphere(0.08, 10, 10);
+    glutSolidSphere(0.4, 10, 10);
+    glPopMatrix();
     
-    //glScalef(0.1, 0.1, 0.0);
-    //glutWireIcosahedron();
+//    glScalef(0.1, 0.1, 0.0);
+//    glutWireIcosahedron();
     
     // Draw Host Cell Membrane
-    glTranslated(0, -0.8, 0);
-    glColor3f(1.0, 0.8, 0.3);       // Orange/Yellow - 1/0.8/0.3
-    glutSolidSphere(0.5, 50, 50);
-    
-    glTranslated(0.0, 0.0, -0.2);
-    glColor3f(0.5, 0.8, 1.0);       // Blue/Green - 0.5, 0.8, 1.0
-    glutSolidSphere(0.48, 50, 50);
+    //--------------------------
+    //
+    // --- Outer Membrane --- Orange Sphere
+    //
+    glPushMatrix();
+    //glEnable(GL_BLEND);  // Enable Blending
+    //glBlendFunc(GL_SRC_ALPHA,GL_ONE_MINUS_SRC_ALPHA);
+    glTranslated(-0.8, -0.8, 0);
+    glColor4f(1.0f, 0.8f, 0.3f, 0.1f);       // Orange/Yellow - 1/0.8/0.3
+    glutSolidSphere(4, 50, 50);
+    //glDisable(GL_BLEND); // Disable Blending
+    glPopMatrix();
+    //--------------------------
+    //
+    // --- Inner Membrane --- Blue Sphere
+    //
+    glPushMatrix();
+    glTranslated(-1.2, -0.2, 2);
+    glColor4f(0.5, 0.8, 1.0, 0.5);       // Blue/Green - 0.5, 0.8, 1.0
+    glutSolidSphere(3.5, 50, 50);
+    glPopMatrix();
+    //--------------------------
     
     // Draw Host Cell Nucleus
-    glTranslated(0.15, -0.2, -0.6);
-    glColor3f(0.7, 1.0, 1.0);       // Pink/Red - 1/0/0.5 - Light Blue - 0.7/1/1
-    glutSolidSphere(0.18, 20, 50);
+    //--------------------------
+    //
+    // --- Light Blue Sphere ---
+    //
+    glPushMatrix();
+    glTranslated(-1.2, -0.06, 6);
+    glColor3f(0.7, 1.0, 1.0);       // Light Blue - 0.7/1/1
+    glutSolidSphere(1.8, 20, 50);
+    glPopMatrix();
+    //--------------------------
     
     glFlush();                                           // Render the scene
     
@@ -103,23 +167,77 @@ void display(){
     
 }
 
-/*
- * RESHAPE
- */
-void reshape(int width, int height){
+//
+// ------ ARROWS ------
+//
+void arrows(int key, int x, int y) {
     
-    //glMatrixMode(GL_PROJECTION);
-    //glLoadIdentity();
-    //gluPerspective(40.0, (GLdouble)width/(GLdouble)height, 0.5, 20.0);
-    //glMatrixMode(GL_MODELVIEW);
+    if (key == GLUT_KEY_RIGHT)            // Right arrow - increase by 2 degree
+        angle += 2;
+
+    else if (key == GLUT_KEY_LEFT)        // Left arrow - decrease by 2 degree
+        angle -= 2;
     
-    ratio = (double)width / (double)height;
-    glViewport(0, 0, width, height);
+    else if (key == GLUT_KEY_UP)          // Up arrow - increase by 5 degree
+        elev += 5;
+    
+    else if (key == GLUT_KEY_DOWN)        // Down arrow - decrease by 5 degree
+        elev -= 5;
+    
+    View();                               // Reset View
+    
+    glutPostRedisplay();
 }
 
-/*
- *   MAIN
- */
+//
+//  ------- KEYS -------
+//
+void keys(unsigned char key, int x, int y)
+{
+  if (key == 27)                          // Esc key - Exit the window
+      exit(0);
+  
+  else if (key == '0')                    // 0 - Reset
+     angle = elev = 0;
+    
+  else if (key == 'v')                    // Switch View - Ortho vs. Pers
+     modeV = 1 - modeV;
+    
+  else if (key == '1'){                   // Zoom In
+    field += 5;
+    view_ortho -= 0.1;
+  }
+    
+  else if (key == '2'){                   // Zoom Out
+    field -= 5;
+    view_ortho += 0.1;
+  }
+
+   View();                                // Reset View
+    
+   glutPostRedisplay();                   // Redisplay normal plane
+}
+
+//
+// ------ RESHAPE ------
+//
+void reshape(int width, int height){
+    
+//    glMatrixMode(GL_PROJECTION);
+//    glLoadIdentity();
+//    gluPerspective(40.0, (GLdouble)width/(GLdouble)height, 0.5, 20.0);
+//    glMatrixMode(GL_MODELVIEW);
+    
+    ratio = (double)width / (double)height;
+    
+    glViewport(0, 0, width, height);
+    
+    View();                                   // ORTHO vs. PERS
+}
+
+//
+// ------ MAIN ------
+//
 int main(int argc, char * argv[]) {
     
     glutInit(&argc,argv);                                // Initialize GLUT
@@ -134,16 +252,13 @@ int main(int argc, char * argv[]) {
     
     glutReshapeFunc(reshape);                            // Reshape Window
     
+    glutKeyboardFunc(keys);                              // Set Window's keys callback
+       
+    glutSpecialFunc(arrows);                             // Special keys
+    
     glutMainLoop();
     
     return 0;
-
 }
 
-
-
-
-
-
-
-
+// ================================================================================================
